864296b9c410003655c0cfcc099bcaf0ca22c1e8
srini88 <pvsrinivas.ece@gmail.com>
2016-01-10 00:11:51 -0600

    added full validation FN, LN, email, PWD (2 views)

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,3 +1,12 @@
 myApp.controller('RegistrationController',['$scope', function($scope){
-	$scope.message = "Welcome to my App";
+	$scope.login = function(){
+		// this message will be printed from the login.html page
+		$scope.message = "Welcome "+ $scope.user.email;
+	};
+	//register is the function frmo the register.html page
+	$scope.register = function(){
+		$scope.message = "Welcome "+ $scope.user.firstname
+		+ $scope.user.lastname+ $scope.user.password;
+	};
+
 }]);
