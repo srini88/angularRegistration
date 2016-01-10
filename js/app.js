fea24cf80b7b33e5022ff97d4a26b6ca934e2a5c
srini88 <pvsrinivas.ece@gmail.com>
2016-01-09 23:12:25 -0600

    setup 3 different routes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-// first arg is name of controller, second array is the dependencies 
-myApp.controller('appController', ['$scope', function($scope){
-	$scope.message = "Welcome to my App";
-}]);
+// remember using config method since using routes
 
-// one app - two different controllers 
-myApp.controller('appController2', ['$scope', function($scope){
-	$scope.message = "message from my second controller";
+// when somebody goes to login section - go to this template
+// also specify a controller for this goddammn view (tie route and controller)
+myApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+	when('/login', {
+		templateUrl:'views/login.html',
+		controller: 'RegistrationController'
+	}).
+	when('/register', {
+		templateUrl:'views/register.html',
+		controller:'RegistrationController'
+	}).
+	when('/success',{
+		templateUrl:'views/success.html',
+		controller:'SuccessController'
+	}).
+	otherwise({
+		redirectTo:'/login'
+	});
 }]);
